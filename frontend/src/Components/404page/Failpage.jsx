310120705e177e18212a4917604da9b0d1fb173c
hoangson2005 <hoangson20051998@gmail.com>
2023-11-25 15:40:38 +0700

    fix 404page

--- frontend/src/Components/404page/Failpage.jsx
+++ frontend/src/Components/404page/Failpage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Failpage.css'
-import fail_img from '../Assets/doona404page.png'
+import fail_img from '../Assets/404page.png'
 
 const Failpage = () => {
   return (
     <div className='failpage'>
       <img src={fail_img} alt="" />
-      <h1>404 ERROR Page !!!</h1>
+      <h1>Please ! Go Back !</h1>
     </div>
   )
 }
