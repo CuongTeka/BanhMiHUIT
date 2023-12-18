6a40b98f9b8ae2e9f869692637f96f903d7bd730
hoangson2005 <hoangson20051998@gmail.com>
2023-12-18 11:00:55 +0700

    update css & paymentpage

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -33,78 +33,80 @@ const CartItems = () => {
     Object.values(cartItems).every((quantity) => quantity === 0);
   // kiem tra gio hang
   return (
-    <div className="cartitems">
-      <div className="cartitems-fomart-main">
-        <p>Sản Phẩm</p>
-        <p>Tiêu Đề</p>
-        <p>Giá Tiền</p>
-        <p>Số lượng</p>
-        <p>Tổng</p>
-        <p>Xoá</p>
-      </div>
-      <hr />
-      {products.map(
-        (e) =>
-          cartItems[e._id] > 0 && (
-            <div key={e._id}>
-              <div className="cartitems-format cartitems-fomart-main">
-                <img
-                  className="carticon-product-icon"
-                  src={`http://localhost:8080/api/images?imageName=${encodeURIComponent(
-                    e.image
-                  )}`}
-                  alt=""
-                />
-                <p>{e.name}</p>
-                <p>{numberFormat(e.price)}</p>
-                <button onClick={() => addToCart(e._id)}>
-                  {cartItems[e._id]}
-                </button>
-                <p>{numberFormat(e.price * cartItems[e._id])}</p>
-                <img
-                  onClick={() => removeToCart(e._id)}
-                  className="cartitems-remove-icon"
-                  src={remove_icon}
-                  alt=""
-                />
+    <div className="cartitems-container">
+      <div className="cartitems">
+        <div className="cartitems-fomart-main">
+          <p>Sản Phẩm</p>
+          <p>Tiêu Đề</p>
+          <p>Giá Tiền</p>
+          <p>Số lượng</p>
+          <p>Tổng</p>
+          <p>Xoá</p>
+        </div>
+        <hr />
+        {products.map(
+          (e) =>
+            cartItems[e._id] > 0 && (
+              <div key={e._id}>
+                <div className="cartitems-format cartitems-fomart-main">
+                  <img
+                    className="carticon-product-icon"
+                    src={`http://localhost:8080/api/images?imageName=${encodeURIComponent(
+                      e.image
+                    )}`}
+                    alt=""
+                  />
+                  <p>{e.name}</p>
+                  <p>{numberFormat(e.price)}</p>
+                  <button onClick={() => addToCart(e._id)}>
+                    {cartItems[e._id]}
+                  </button>
+                  <p>{numberFormat(e.price * cartItems[e._id])}</p>
+                  <img
+                    onClick={() => removeToCart(e._id)}
+                    className="cartitems-remove-icon"
+                    src={remove_icon}
+                    alt=""
+                  />
+                </div>
+                <hr />
+              </div>
+            )
+        )}
+        <div className="cartitems-down">
+          <div className="cartitems-total">
+            <h1>Tổng Tiền Giỏ Hàng</h1>
+            <div>
+              <div className="cartitems-total-item">
+                <p>Tổng số tiền hàng</p>
+                <p>{numberFormat(getTotalCartAmount())}</p>
               </div>
               <hr />
+              <div className="cartitems-total-item">
+                <p>Phí giao hàng</p>
+                <p>Free</p>
+              </div>
+              <hr />
+              <div className="cartitems-total-item">
+                <h3>Tổng </h3>
+                <h3>{numberFormat(getTotalCartAmount())}</h3>
+              </div>
             </div>
-          )
-      )}
-      <div className="cartitems-down">
-        <div className="cartitems-total">
-          <h1>Tổng Tiền Giỏ Hàng</h1>
-          <div>
-            <div className="cartitems-total-item">
-              <p>Tổng số tiền hàng</p>
-              <p>{numberFormat(getTotalCartAmount())}</p>
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <p>Phí giao hàng</p>
-              <p>Free</p>
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <h3>Tổng </h3>
-              <h3>{numberFormat(getTotalCartAmount())}</h3>
-            </div>
-          </div>
 
-          {isLoggedIn ? (
-            <button onClick={handlePaymentClick}>TIẾN HÀNH THANH TOÁN</button>
-          ) : (
-            <Link style={{ textDecoration: "none" }} to="/signin">
-              <button>ĐĂNG NHẬP ĐỂ THANH TOÁN</button>
-            </Link>
-          )}
-        </div>
-        <div className="cartitems-promocode">
-          <p>Nếu bạn có mã giảm giá, Điền tại đây !</p>
-          <div className="cartitems-promobox">
-            <input type=" " placeholder="Nhập mã giảm giá" />
-            <button>Gửi Mã</button>
+            {isLoggedIn ? (
+              <button onClick={handlePaymentClick}>TIẾN HÀNH THANH TOÁN</button>
+            ) : (
+              <Link style={{ textDecoration: "none" }} to="/signin">
+                <button>ĐĂNG NHẬP ĐỂ THANH TOÁN</button>
+              </Link>
+            )}
+          </div>
+          <div className="cartitems-promocode">
+            <p>Nếu bạn có mã giảm giá, Điền tại đây !</p>
+            <div className="cartitems-promobox">
+              <input type=" " placeholder="Nhập mã giảm giá" />
+              <button>Gửi Mã</button>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/Pages/PaymentPage.jsx
+++ frontend/src/Pages/PaymentPage.jsx
@@ -8,15 +8,14 @@ const PaymentPage = () => {
   const [paymentMethod, setPaymentMethod] = useState(""); // Chon phuong thuc thanh toan
   const navigate = useNavigate();
   const { resetCart } = useContext(ShopContext);
-  const { getTotalCartAmount, products, cartItems, addToCart } =
-    useContext(ShopContext);
+  const { getTotalCartAmount, products, cartItems } = useContext(ShopContext);
 
   const handlePayment = () => {
     if (paymentMethod === "cash") {
-      // Implement cash payment logic here
+      
       alert("Thanh toán tiền mặt thành công!");
     } else if (paymentMethod === "online") {
-      // Implement online payment logic here
+      
       alert("Thanh toán trực tuyến thành công!");
     }
     resetCart();
@@ -32,7 +31,6 @@ const PaymentPage = () => {
           <p>Giá Tiền</p>
           <p>Số lượng</p>
           <p>Tổng</p>
-          <p>Xoá</p>
         </div>
         <hr />
         {products.map(
@@ -49,9 +47,7 @@ const PaymentPage = () => {
                   />
                   <p>{e.name}</p>
                   <p>{numberFormat(e.price)}</p>
-                  <button onClick={() => addToCart(e._id)}>
-                    {cartItems[e._id]}
-                  </button>
+                  <p>{cartItems[e._id]}</p>
                   <p>{numberFormat(e.price * cartItems[e._id])}</p>
                 </div>
                 <hr />
@@ -59,7 +55,7 @@ const PaymentPage = () => {
             )
         )}
         <div className="paymentcartitems-down">
-          <div className="paymentcartitems-total">
+          <div className="paymentcartitems-downleft">
             <h1>Tổng Tiền Giỏ Hàng</h1>
             <div>
               <div className="paymentcartitems-total-item">
@@ -78,30 +74,41 @@ const PaymentPage = () => {
               </div>
             </div>
           </div>
-          <h1 className="payment-title">Chọn phương thức thanh toán</h1>
-          <div className="payment-options">
-            <label className="payment-option-label">
-              <input
-                type="radio"
-                value="cash"
-                checked={paymentMethod === "cash"}
-                onChange={() => setPaymentMethod("cash")}
-              />
-              Thanh toán tiền mặt
-            </label>
-            <label className="payment-option-label">
-              <input
-                type="radio"
-                value="online"
-                checked={paymentMethod === "online"}
-                onChange={() => setPaymentMethod("online")}
-              />
-              Thanh toán trực tuyến
-            </label>
+          <div className="paymentcartitems-downright">
+            <h1 className="payment-title">Chọn phương thức thanh toán</h1>
+            <div className="payment-options">
+              <label className="payment-option-label">
+                <input
+                  type="radio"
+                  value="cash"
+                  checked={paymentMethod === "cash"}
+                  onChange={() => setPaymentMethod("cash")}
+                />
+                Thanh toán tiền mặt
+              </label>
+              <label className="payment-option-label">
+                <input
+                  type="radio"
+                  value="online"
+                  checked={paymentMethod === "online"}
+                  onChange={() => setPaymentMethod("online")}
+                />
+                Thanh toán VnPay
+              </label>
+              <label className="payment-option-label">
+                <input
+                  type="radio"
+                  value="online"
+                  checked={paymentMethod === "online"}
+                  onChange={() => setPaymentMethod("online")}
+                />
+                Thanh toán MoMo
+              </label>
+            </div>
+            <button className="payment-button" onClick={handlePayment}>
+              Xác nhận thanh toán
+            </button>
           </div>
-          <button className="payment-button" onClick={handlePayment}>
-            Xác nhận thanh toán
-          </button>
         </div>
       </div>
     </div>
